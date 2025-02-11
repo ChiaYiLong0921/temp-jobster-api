3627182ea883ff5119b5f8bd0d2161190bc9f99e
Eric <e_chia0921@outlook.com>
2025-02-11 19:54:19 +0800

    fix connection

---
+++ app.js
@@ -0,0 +1,64 @@
+require('dotenv').config()
+require('express-async-errors')
+
+const path = require('path')
+
+// extra security packages
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
+// routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+// app.set('trust proxy', 1)
+
+// app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.json())
+app.use(helmet())
+app.use(xss())
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'], // Allow only requests from this origin
+  methods: 'GET,POST,PATCH,DELETE', // Allow only these methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
+}
+
+// Use CORS middleware with specified options
+app.use(cors(corsOptions))
+
+// routes
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+app.get('*', (req, res) => {
+  res.send('jobster')
+  // res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
