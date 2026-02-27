b4dec5b97881799ce04036c43436a1f9f113ed6b
Eric <e_chia0921@outlook.com>
2026-02-27 11:41:59 +0800

    cleanup

--- app.js
+++ app.js
@@ -26,9 +26,8 @@ const statusRouter = require("./routes/status")
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
-// app.set('trust proxy', 1)
+app.set('trust proxy', 1)
 
-// app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use(express.json())
 app.use(helmet())
 app.use(xss())
@@ -49,12 +48,8 @@ app.use('/api/v1/status', statusRouter)
 
 app.get('*', (req, res) => {
   res.send('jobster')
-  // console.log(req.headers)
-
-  // res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
 })
 
-app.get('/api/v1/health', );
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
